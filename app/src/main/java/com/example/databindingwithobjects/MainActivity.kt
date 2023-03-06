32d91a0717e641f259830d0d4298b296839b830f
DESKTOP-N4VRD92\Fujitsu <vidojemuric@gmail.com>
2023-03-06 15:40:57 +0300

    First commit

---
+++ app/src/main/java/com/example/databindingwithobjects/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.databindingwithobjects
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databindingwithobjects.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        var u1: User = User(1, "Jack", "123")
+
+        binding.user = u1
+    }
+}
